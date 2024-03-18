4e169a6c93ef9c3a1e0b7d03d08cb2c9e89df128
Charuhas Reddy <charuhasreddybalam@gmail.com>
2024-03-18 13:05:57 +0530

    changed vite config

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+const base =
+  process.env.NODE_ENV === "production" ? "/charuhas10.github.io/" : "/";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode basename={base}>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
