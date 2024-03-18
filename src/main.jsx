b931accce88117ea9201237a60379e048a4c3597
Charuhas Reddy <charuhasreddybalam@gmail.com>
2024-03-18 14:35:54 +0530

    Removed imports

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import process from "process";
 
 const base =
   process.env.NODE_ENV === "production" ? "/charuhas10.github.io/" : "/";
