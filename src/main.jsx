a750e63182a993dfaada34300b8b1bf31687be95
Charuhas Reddy <charuhasreddybalam@gmail.com>
2024-03-18 15:20:34 +0530

    fixes

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-const base = "/charuhas10.github.io/";
+// const base = "/charuhas10.github.io/";
 // const base =
 // process.env.NODE_ENV === "production" ? "/charuhas10.github.io/" : "/";
+// basename={base}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode basename={base}>
+  <React.StrictMode>
     <App />
   </React.StrictMode>
 );
