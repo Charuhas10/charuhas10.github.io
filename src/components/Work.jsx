47397d0af5e0b4dec3c25c14ff179f45a2da09f1
Charuhas Reddy <charuhasreddybalam@gmail.com>
2024-03-22 00:31:29 +0530

    Update project descriptions

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -7,7 +7,7 @@ export default function Work() {
       image: "/LLM.jpeg",
       title: "LLM-NET",
       description:
-        "An application that showcases the various models deployed by organisations and developers alike. Built using NEXT.JS and MongoDB, it utilizes  ",
+        "LLM Net is a dynamic application that showcases the various Large Language Models deployed by organisations and developers alike. Built using NEXT.JS and MongoDB, this platform offers seamless switching between light and dark themes, ensuring a comfortable viewing experience for all users.  ",
       link: "https://llm-net.vercel.app/",
     },
     {
@@ -26,27 +26,27 @@ export default function Work() {
       image: "/moviememo.jpeg",
       title: "MOVIE MEMO",
       description:
-        "MovieMemo is a sleek and user-friendly web application designed to help movie enthusiasts keep track of the films they want to watch",
+        "MovieMemo is a sleek and user-friendly web application designed to help Movie enthusiasts keep track of the films they want to watch. The application is built using HTML, CSS, JavaScript and Firebase.",
       link: "https://movie-memo.netlify.app/",
     },
     {
       image: "/plotforge.jpeg",
       title: "PLOTFORGE",
       description: "Description of Project 3",
-      link: "https://github.com/Charuhas10/PlotForge",
+      link: "This is a Web Application that allows users to create interesting Movie plots with a title and an AI generated poster by just simply entering an Outline of the plot by leveraging the OpenAI API. The application is built using ReactJS and Flask Backend.",
     },
     {
       image: "/etch.jpeg",
       title: "ETCH-A-SKETCH",
       description:
-        "The Etch-a-Sketch project is a web-based implementation of the classic toy that allows users to draw and create artwork using their mouse. It provides a nostalgic experience while embracing modern web technologies.",
+        "The Etch-a-Sketch project is a web-based implementation of the classic toy that allows users to draw and create artwork using their mouse. It provides a nostalgic experience while embracing modern web technologies. The project is built using HTML, CSS, and JavaScript.",
       link: "https://charuhas10.github.io/Etch-a-Sketch/",
     },
     {
       image: "/chromaShift.jpeg",
       title: "CHROMA SHIFT",
       description:
-        "ChromaShift is a simple web application that allows users to input any color in hex code and provides them with the option to lighten or darken the color",
+        "ChromaShift is a simple web application that allows users to input any color in hex code and provides them with the option to lighten or darken the color. The application is built using HTML, CSS, and JavaScript.",
       link: "https://charuhas10.github.io/ChromaShift/",
     },
   ];
